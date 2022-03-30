7ce39b47dd6a81430641fcd6b051890a4d20d75f
Andrew Graham <andrew.s.graham15.civ@army.mil>
2022-03-30 15:06:39 -0500

    Updating exercise 4 solution.

--- solutions/exercise-4.sh
+++ solutions/exercise-4.sh
@@ -3,7 +3,7 @@
 # Created a bundle folder so we can a .imgpkg folder as expected
 imgpkg push -b docker.io/jklhgfcm/imgpkg-class:ex4  -f bundle -f bundleme.yaml
 
-# Alternative is to copy the bundleme.yaml into the bundle/ directory.
+# Alternative is to copy the bundleme.yaml into the bundle/ directory and get rid of the -f bundleme.yaml.
 
 # A good practice is to stick a --lock-output cli flag as well
 # Future pulls be sure to use the lock-output file!
